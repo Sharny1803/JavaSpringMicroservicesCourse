090d4551c4556cd26847aee70dfefb78d3630529
Michal Kalisiak <michal.kalisiak@globallogic.com>
2023-01-30 16:37:33 +0100

    Switching to JPA

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,18 +1,27 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class User {
+
+    @Id
+    @GeneratedValue
     private Integer id;
-    @JsonProperty("user_name")
+//    @JsonProperty("user_name")
     @Size(min = 2, message = "Name should have at least 2 characters.")
     private String name;
     @Past(message = "Birth date should be in the past.")
     private LocalDate birthDate;
+    protected User() {
+    }
 
     public User(Integer id, String name, LocalDate birthDate) {
         this.id = id;
